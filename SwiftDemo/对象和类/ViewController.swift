0dd4874e90b2793ce10d05d4716b343e6072a7b1
杨冬武 <apple@DWdeiMac.local>
2017-10-11 17:59:36 +0800

    对象和类的初识

---
+++ SwiftDemo/对象和类/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  对象和类
+//
+//  Created by Apple on 2017/10/11.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let shape = Shape.init()
+        
+        print(shape.a)
+        
+        shape.mesthed1(num: shape.a)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+    
+    let a = 11
+    private func mesthed1(num:Int) {
+        print(num)
+    }
+}
