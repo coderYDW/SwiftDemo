040e677a30a69a8277bdcff6d25e281a62be9618
杨冬武 <apple@DWdeiMac.local>
2017-10-12 18:01:23 +0800

    创建协议和扩展

---
+++ SwiftDemo/协议和扩展/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  协议和扩展
+//
+//  Created by Apple on 2017/10/12.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //TODO: 学习协议和和扩展  19页
+        //FIXME: JJJ
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
