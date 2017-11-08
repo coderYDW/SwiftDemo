dbe5d1a5b2f9ead819929e1ca19153aed662b39b
DovYoung <1591759055@qq.com>
2017-11-08 23:04:47 +0800

    协议

--- SwiftDemo/协议和扩展/ViewController.swift
+++ SwiftDemo/协议和扩展/ViewController.swift
@@ -8,13 +8,69 @@
 
 import UIKit
 
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust()
+}
+
+class simpleClass: ExampleProtocol {
+    
+    var simpleDescription: String = "a vary simple calss"
+    var anotherProperty: Int = 100;
+    
+    func adjust() {
+        simpleDescription += "Now 100% adjust"
+    }
+    
+}
+
+struct simpleStruct: ExampleProtocol {
+    var simpleDescription: String = "a simple struct"
+    mutating func adjust() {
+        simpleDescription += "(adjusted)"
+    }
+    
+}
+
+enum simpleEnem: ExampleProtocol {
+    case result(String)
+    case failure(String)
+    
+    var simpleDescription: String {
+        get {
+            return self.simpleDescription
+        }
+        set {
+            
+        }
+    }
+    
+    func adjust() {
+        
+    }
+    
+    
+}
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //TODO: 学习协议和和扩展  19页
-        //FIXME: JJJ
+        let a = simpleClass()
+        a.adjust()
+        let aDescription = a.simpleDescription
+        print(aDescription)
+        
+        var b = simpleStruct()
+        b.adjust()
+        print(b.simpleDescription)
+        
+        //TODO: - 19页学习延展
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
